6f3df9707a6fd851cd7f81351a804d7a6573d674
sailaja gorsupudi <sailaja.gorsupudi19@gmail.com>
2017-11-01 11:48:33 +0530

    completed the interface

--- onlineshopping/src/main/java/com/online/onlineshopping/controller/pageController.java
+++ onlineshopping/src/main/java/com/online/onlineshopping/controller/pageController.java
@@ -11,7 +11,8 @@ public class pageController {
 	public ModelAndView index()
 	{
 		ModelAndView mv=new ModelAndView("page");
-		mv.addObject("greeting","welcome to spring web mvc");
+		mv.addObject("title","Home");
+		mv.addObject("userClickHome","true");
 		return mv;
 	}
 	
@@ -26,18 +27,34 @@ public class pageController {
 		//return mv;
 		
 	//}
+	@RequestMapping(value= "/about")
+	public ModelAndView about()
+	{
+		ModelAndView mv=new ModelAndView("page");
+		mv.addObject("title","About Us");
+		mv.addObject("userClickAbout","true");
+		return mv;
+	}
+	@RequestMapping(value= "/contact")
+	public ModelAndView contact()
+	{
+		ModelAndView mv=new ModelAndView("page");
+		mv.addObject("title","contact Us");
+		mv.addObject("userClickContact","true");
+		return mv;
+	}
 	
-	@RequestMapping(value="/test/{greeting}")
-		public ModelAndView test(@PathVariable("greeting")String greeting) {
-			if(greeting==null)
-			{
-				greeting="Hello there";
-			}
-			ModelAndView mv=new ModelAndView("page");
-			mv.addObject("greeting",greeting);
-			return mv;
+	//@RequestMapping(value="/test/{greeting}")
+		//public ModelAndView test(@PathVariable("greeting")String greeting) {
+		//	if(greeting==null)
+		//	{
+			//	greeting="Hello there";
+		//	}
+		//	ModelAndView mv=new ModelAndView("page");
+		//	mv.addObject("greeting",greeting);
+		//	return mv;
 			
-		}
+		//}
 	
 
 }
---
+++ onlineshopping/src/main/webapp/WEB-INF/views/shared/navbar.jsp
@@ -0,0 +1,26 @@
+ <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+      <div class="container">
+        <a class="navbar-brand" href="${contextRoot}/home">online shopping</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="sr-only">Togggle navigation</span>
+          <span class="icon-bar"></span>
+           <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+           
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+            <li id="about">
+              <a href="${contextRoot}/about">About</a>
+            </li>
+            <li id="listProducts">
+              <a  href="${contextRoot}/listProducts">View Products</a>
+            </li>
+            <li id="contact">
+              <a  href="${contextRoot}/contact">Contact</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
